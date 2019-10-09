1d09dab75a38bb2bfe92818a4d4cd8e8ecd0c428
ThanhSonITNIC <ThanhSon.ITNIC@gmail.com>
2019-10-09 22:53:46 +0700

    update menu admin

--- resources/views/admin/layouts/menu.blade.php
+++ resources/views/admin/layouts/menu.blade.php
@@ -11,36 +11,12 @@
             <li class=" nav-item"><a href="/admin"><i class="icon-home3"></i><span data-i18n="nav.form_layouts.form_layout_basic" class="menu-title">Dashboard</span></a>
             </li>
             <li class=" nav-item"><a href="/admin/users"><i class="icon-person-stalker"></i><span data-i18n="nav.cards.main" class="menu-title">Users</span></a>
-                <ul class="menu-content">
-                    <li>
-                        <a href="/admin/users" data-i18n="nav.cards.card_bootstrap" class="menu-item">List</a>
-                    </li>
-                    <li>
-                        <a href="/admin/users/1" data-i18n="nav.cards.card_actions" class="menu-item">Show</a>
-                    </li>
-                </ul>
             </li>
             <li class=" nav-item"><a href="/admin/products"><i class="icon-tshirt"></i><span data-i18n="nav.cards.main" class="menu-title">Apparels</span></a>
-                <ul class="menu-content">
-                    <li>
-                        <a href="/admin/products" data-i18n="nav.cards.card_bootstrap" class="menu-item">List</a>
-                    </li>
-                    <li>
-                        <a href="/admin/products/1" data-i18n="nav.cards.card_actions" class="menu-item">Show</a>
-                    </li>
-                </ul>
             </li>
             <li class=" nav-item"><a href="/admin/orders"><i class="icon-ios-paper"></i><span data-i18n="nav.form_layouts.form_layout_basic" class="menu-title">Orders</span></a>
             </li>
             <li class=" nav-item"><a href="/admin/posts"><i class="icon-news"></i><span data-i18n="nav.cards.main" class="menu-title">Posts</span></a>
-                <ul class="menu-content">
-                    <li>
-                        <a href="/admin/posts" data-i18n="nav.cards.card_bootstrap" class="menu-item">List</a>
-                    </li>
-                    <li>
-                        <a href="/admin/posts/1" data-i18n="nav.cards.card_actions" class="menu-item">Show</a>
-                    </li>
-                </ul>
             </li>
             <li class=" nav-item"><a href="/admin/settings"><i class="icon-settings"></i><span data-i18n="nav.cards.main" class="menu-title">Settings</span></a>
                 <ul class="menu-content">
